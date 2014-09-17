a6c072df0cf4ced6b3bc6ce72a10110080c554be
LittleWhite <lw.demoscene@googlemail.com>
2014-09-17 23:47:45 +0200

    Some work on game loop and timing, but did not get a nice result yet

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
+#include <iostream>
+
 #include "Settings.hpp"
 
 #include "Barrier.hpp"
@@ -17,10 +19,17 @@ int main()
     Player p(sf::Vector2f(100,100));
     Enemy e(sf::Vector2f(WIN_WIDTH/2,WIN_HEIGHT/2));
 	
+    sf::Clock gameClock;
+    sf::Time elapsedTime = sf::Time::Zero;
+    sf::Time previousStartTime  = gameClock.getElapsedTime();
+
 	while (window.isOpen())
     {
+        elapsedTime = gameClock.restart() - previousStartTime;
+
+        // TODO : Find better to catch window closure, or
+        // filter most event to not take too much time here (mouse events)
         sf::Event event;
-        // This is buggy, mouse is impacting player speed
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
@@ -28,35 +37,40 @@ int main()
                 window.close();
                 break;
             }
-                
-            window.clear(sf::Color::Black);
-
-            // First try with input.
-            // TODO : better input, should take time between two iteration
-            // TODO : having a progressive system (aka acceleration) on key press
-            if ( sf::Keyboard::isKeyPressed(sf::Keyboard::Left) )
-            {
-                p.move(sf::Vector2f(-1.0,0.0) * Settings::playerSpeed);
-            }
-            if ( sf::Keyboard::isKeyPressed(sf::Keyboard::Right) )
-            {
-                p.move(sf::Vector2f(1.0,0.0)  * Settings::playerSpeed);
-            }
-            if ( sf::Keyboard::isKeyPressed(sf::Keyboard::Up) )
-            {
-                p.move(sf::Vector2f(0.0,-1.0) * Settings::playerSpeed);
-            }
-            if ( sf::Keyboard::isKeyPressed(sf::Keyboard::Down) )
-            {
-                p.move(sf::Vector2f(0.0,1.0) * Settings::playerSpeed);
-            }
-
-            b.draw(window);
-            // e.draw(window);
-            p.draw(window);
-
-			window.display();
         }
+                
+        window.clear(sf::Color::Black);
+
+        // First try with input.
+        // TODO : better input, should take time between two iteration
+        // TODO : having a progressive system (aka acceleration) on key press
+        // float updateSpeed = elapsedTime.asSeconds() * 1000;
+        float updateSpeed = 1;
+        if ( sf::Keyboard::isKeyPressed(sf::Keyboard::Left) )
+        {
+            p.move(sf::Vector2f(-1.0,0.0) * updateSpeed * Settings::playerSpeed);
+        }
+        if ( sf::Keyboard::isKeyPressed(sf::Keyboard::Right) )
+        {
+            p.move(sf::Vector2f(1.0,0.0) * updateSpeed * Settings::playerSpeed);
+        }
+        if ( sf::Keyboard::isKeyPressed(sf::Keyboard::Up) )
+        {
+            p.move(sf::Vector2f(0.0,-1.0) * updateSpeed * Settings::playerSpeed);
+        }
+        if ( sf::Keyboard::isKeyPressed(sf::Keyboard::Down) )
+        {
+            p.move(sf::Vector2f(0.0,1.0) * updateSpeed * Settings::playerSpeed);
+        }
+
+        b.draw(window);
+        // e.draw(window);
+        p.draw(window);
+
+        window.display();
+
+        previousStartTime = gameClock.getElapsedTime();
+        std::cout << "Elapsed time (s): " << elapsedTime.asSeconds() << std::endl;
     }
 	
 	return 0;
