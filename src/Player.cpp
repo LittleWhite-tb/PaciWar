30792577952c82ccb25b5e8f062eb67b88abccfe
LittleWhite <lw.demoscene@googlemail.com>
2015-03-12 18:54:13 +0100

    Trying to fix same issue for MinGW

--- src/Player.cpp
+++ src/Player.cpp
@@ -2,6 +2,11 @@
 
 #define _USE_MATH_DEFINES
 #include <cmath>
+#ifndef M_PI
+    // hacky but well MinGW did want to see the symbol
+    # define M_PI		3.14159265358979323846	/* pi */
+#define
+
 #include <limits>
 
 #include "Math/Interpolation.hpp"
