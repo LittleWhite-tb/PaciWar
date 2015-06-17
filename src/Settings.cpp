042489e1fad164e57d7c055c0558fd2d150dd3b4
LittleWhite <lw.demoscene@googlemail.com>
2015-06-18 00:02:12 +0200

    Add version print in title bar

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -1,6 +1,8 @@
 #include "Settings.hpp"
 
-const std::string Settings::windowName = "PaciWar";
+#include "version.hpp"
+
+const std::string Settings::windowName = "PaciWar v" VERSION_MAJOR "." VERSION_MINOR "." VERSION_PATCH " - " VERSION_SHA;
 
 const std::string Settings::fontPath = "./data/DejaVuSansMono.ttf";
 #if DEBUG_INFO == 1
