afcc964feebdcdfa36f7c58919aa331c940db5a8
Alexandre LittleWhite Laurent <lw.demoscene@googlemail.com>
2016-08-19 15:01:00 +0200

    Implement base class Object

---
+++ src/Objects/Object.hpp
@@ -0,0 +1,30 @@
+/*
+ * PaciWar : Remake of the "pacifism" mode from Geometry Wars 2
+ * Copyright (C) 2014-2016 LittleWhite (lw.demoscene@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class Object
+{
+private:
+protected:
+    std::string m_id;
+    std::string m_classId;
+
+public:
+    Object(const std::string& id, const std::string& classId)
+        :m_id(id),m_classId(classId) {}
+
+};
